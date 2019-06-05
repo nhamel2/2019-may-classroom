d66705713b3d71c4a4a95977645ceaf914e74fc3
nhamel2 <nhamel2@nait.ca>
2019-06-05 16:52:16 -0600

    class 013

---
+++ 003/new/CSNet/WebApp/SamplePages/Hello.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp.SamplePages
+{
+    public partial class Hello : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            MessageArea.Text = "whatever i want";
+        }
+
+        protected void Reset_Click(object sender, EventArgs e)
+        {
+            MessageArea.Text = "";
+        }
+
+        protected void PressMe_Click(object sender, EventArgs e)
+        {
+            MessageArea.Text = "Don Welch";
+        }
+    }
+}
