b774e2fd0a8bd600929446cabc3c510ae0c57a55
nhamel2 <nhamel2@nait.ca>
2019-06-07 16:56:47 -0600

    class 014

--- 003/new/CSNet/WebApp/SamplePages/BasicControls.aspx.cs
+++ 003/new/CSNet/WebApp/SamplePages/BasicControls.aspx.cs
@@ -5,16 +5,16 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace WebApp_ASP_.SamplePages
+namespace WebApp.SamplePages
 {
-    public partial class helloworld : System.Web.UI.Page
+    public partial class BasicControls : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            MessageArea.Text = "Hello World!!!";
+
         }
 
-        protected void Submit_Click(object sender, EventArgs e)
+        protected void Button1_Click(object sender, EventArgs e)
         {
 
         }
